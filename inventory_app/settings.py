f1deb25b18edf32bcc91907e01c72684ebf29ff6
arishboteco <arish@boteco.co.in>
2025-08-15 18:22:34 +0530

    Update settings.py

--- inventory_app/settings.py
+++ inventory_app/settings.py
@@ -27,7 +27,7 @@ environ.Env.read_env(BASE_DIR / ".env")
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env("DJANGO_DEBUG")
+DEBUG = env.bool("DJANGO_DEBUG", default=False)
 
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS")
 

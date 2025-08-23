6f1f38997a3937ce7c3333890cfe1f08c7ae38b7
arishboteco <arish@boteco.co.in>
2025-08-23 20:24:02 +0200

    feat: expand layout tokens and container config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,19 @@ module.exports = {
     "./static/js/**/*.js",
   ],
   theme: {
+    container: {
+      center: true,
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px',
+      },
+      padding: {
+        DEFAULT: 'var(--space-8)',
+      },
+    },
     screens: {
       sm: '640px',
       md: '768px',
@@ -14,6 +27,7 @@ module.exports = {
       'max-md': { max: '767px' },
       'max-lg': { max: '1023px' },
       'max-xl': { max: '1279px' },
+      'max-2xl': { max: '1535px' },
     },
     extend: {
       colors: {
@@ -51,6 +65,8 @@ module.exports = {
         '1': 'var(--space-1)',
         '2': 'var(--space-2)',
         '4': 'var(--space-4)',
+        '6': 'var(--space-6)',
+        '8': 'var(--space-8)',
       },
       fontSize: {
         base: 'var(--font-size-base)',

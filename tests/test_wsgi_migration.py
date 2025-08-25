72cc025d2278dd7a27daa2d8b5c5ed624b0bfc64
arishboteco <arish@boteco.co.in>
2025-08-25 10:56:29 +0200

    Stop running migrations in WSGI

--- tests/test_wsgi_migration.py
+++ tests/test_wsgi_migration.py
@@ -2,11 +2,11 @@ from unittest.mock import patch
 import sys
 
 
-def test_wsgi_runs_migrate():
+def test_wsgi_does_not_run_migrate():
     sys.modules.pop("inventory_app.wsgi", None)
     with patch("django.core.management.call_command") as call, patch(
         "django.core.wsgi.get_wsgi_application"
     ):
         import inventory_app.wsgi  # noqa: F401
 
-    call.assert_called_with("migrate", interactive=False)
+    call.assert_not_called()
